8b31247333a3a4b821238cde9acebcd567ebddcf
almeidaxz <almeidaxz@live.com>
2022-11-27 15:40:48 -0300

    fix formatação de data

--- acme-client/src/services/apiConnection.js
+++ acme-client/src/services/apiConnection.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://acme-clinica.up.railway.app',
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json'
