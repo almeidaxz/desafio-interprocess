3bcb0f41332a8532167ddc4cdc4b53f35b233ab0
almeidaxz <almeidaxz@live.com>
2022-11-27 15:35:15 -0300

    fix na sintaxe da função format date

--- acme-server/src/utils/formatDate.js
+++ acme-server/src/utils/formatDate.js
@@ -1,10 +1,12 @@
 const { format } = require('date-fns');
 const { utcToZonedTime } = require('date-fns-tz');
 
-export function formatDateToInput(date) {
+const formatDateToInput = (date) => {
     const newDate = new Date(date)
     const timeZone = 'Europe/Berlin'
     const zonedDate = utcToZonedTime(newDate, timeZone)
 
     return format(zonedDate, 'yyyy-MM-dd');
 }
+
+module.exports = { formatDateToInput };
